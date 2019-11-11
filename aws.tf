731f2be546caa1f2d3c27112b9735407c85900b1
ramkrishnamca <19373347+ramkrishnamca@users.noreply.github.com>
2019-11-11 22:25:30 +0530

    updated
    updated code as per comments

--- aws.tf
+++ aws.tf
@@ -3,7 +3,7 @@
 
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
-variable "AWS_Rama_KEY" {}
+variable "AWS_RAMA_KEY" {}
 
 provider "aws" {
   region = "us-east-1"
@@ -11,7 +11,7 @@ provider "aws" {
   secret_key ="${var.AWS_SECRET_KEY}"
 
 }
-
+/* aws pair creation */
 resource "aws_key_pair" "devopskey" {
   key_name    = "devopskey"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDScKU4LBAPXrumGvAEt8v6qEImlR/DK289BAWKyulaG0ROlb6Bxh1R9z7HYTpPQGviep67Ql0gcT0yPn/BR2A61/Z9tMViqfJLfzZi9s+fzrAfLbuBG2+BbULATiU6l+weqSV0KWl3OLf7BSQA1TWi1UHqAz0X0eyvczyXo5bF/hYy0Ixv2nN2AcW5v1ajIQnQ6hmyDZmT4d/GjmQfnM9iNSjIdtEz7NVN5u0On4wqErZSuB+zcGI1VItvosOXE4f3eUSyv+huwSAdFXPTMq/Xbr2HB/PV6zsuCcO3pnAB0ztmTBIgkBa6aLp2WMya1SavRCXLHdH+J8KIOjb340N7 root@ip-11-0-0-30.eu-central-1.compute.internal"
